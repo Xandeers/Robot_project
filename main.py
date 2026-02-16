767663189ac2ca89fbd1f5a127d5150b25db4db1
Xandeers <nedim.tr.pro@gmail.com>
2026-02-16 23:32:44 +0100

    just updated

--- main.py
+++ main.py
@@ -24,7 +24,8 @@ client,connexion = creer_et_attendre_connexion()
 message= "la connexion est OK"
 envoyer_message=(connexion,message)
 
-print("fermeture socket dans...")
+message2= "fermeture socket dans..."
+envoyer_message(connexion,message2)
 for i in range(10):
     envoyer_message(connexion,i)
     sleep(1)
