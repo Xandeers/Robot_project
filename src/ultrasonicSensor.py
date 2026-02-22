b9666ef1db3bbea5bac87027d205b69d54224866
Xandeers <nedim.tr.pro@gmail.com>
2026-02-22 14:22:56 +0100

    fist function

---
+++ src/ultrasonicSensor.py
@@ -0,0 +1,16 @@
+from ev3dev2.sensor.lego import UltrasonicSensor
+
+DISTMIN=20
+
+#en centimetre
+def getvalue(sensor) :
+
+    return sensor.distance_centimeters
+
+def boolColision(sensor):
+    if(getvalue(sensor <= DISTMIN)): 
+        return True
+    else:
+        return False
+    
+
