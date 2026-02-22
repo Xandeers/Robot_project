075d2cd3d0bf07ac16f9fc6cce013077971e2205
Xandeers <nedim.tr.pro@gmail.com>
2026-02-22 14:35:42 +0100

    test IRL V0 to do

--- main.py
+++ main.py
@@ -6,11 +6,23 @@ from ev3dev2.motor import LargeMotor, OUTPUT_A,OUTPUT_B, OUTPUT_C, OUTPUT_D
 from src.moteur import *
 from src.socket import creer_et_attendre_connexion, envoyer_message, fermer_reseau
 
+from src.ultrasonicSensor import *
+from ev3dev2.sensor import INPUT_4
 
+from ev3dev2.sound import Sound
+
+#motor
 m1= LargeMotor(OUTPUT_A)
 m2= LargeMotor(OUTPUT_B)
 m3= LargeMotor(OUTPUT_C)
 m4= LargeMotor(OUTPUT_D)
+
+#ultrasonCapteur
+sensor = UltrasonicSensor(INPUT_4)
+
+#Speaker
+speaker= Sound()
+
 
 print("Initialisation du robot...")
 
@@ -22,6 +34,15 @@ sleep(5)
 avanceVehicule_Uniform(m1,m2,m3,m4,100)
 sleep(5)
 glissement_Avant_droit(m1,m2,m3,m4)
+
+
+#test colision 
+
+while True : 
+    if (boolColision(sensor)):
+        speaker.beep()
+
+    break
 
 
 #test socket 
