7423db2e8dc296936236aa0f096a9c5c9ed058cb
Xandeers <nedim.tr.pro@gmail.com>
2026-04-27 16:55:18 +0200

    color mask modified

--- camera/Tracker/ball.py
+++ camera/Tracker/ball.py
@@ -5,9 +5,11 @@ import cv2
 
 class BallTracker : 
     def __init__(self):
-        self.lower_red1 = np.array([0, 120, 70])
-        self.upper_red1 = np.array([10, 255, 255])
-        self.lower_red2 = np.array([170, 120, 70])
+
+        self.lower_red1 = np.array([0, 100, 50])   
+        self.upper_red1 = np.array([15, 255, 255]) 
+
+        self.lower_red2 = np.array([160, 100, 50]) 
         self.upper_red2 = np.array([180, 255, 255])
 
     
