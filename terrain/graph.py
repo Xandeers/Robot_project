69fb5f1e06cdeb51286493eff5722effa9464fec
Xandeers <nedim.tr.pro@gmail.com>
2026-04-29 17:08:52 +0200

    add_point added

--- terrain/graph.py
+++ terrain/graph.py
@@ -19,10 +19,18 @@ class Coordonee:
 
 class Graph:
     def __init__(self, x_widthCM=301, y_lengthCM=390):
-
         self.width=x_widthCM
         self.length=y_lengthCM
         self.point=[]
+    
+    
+    def add_point(self,p):
+        if 0<= p.x <= self.width and 0<= p.y <= self.length :
+            self.point.append(p)
+            return True
+        else:
+            print("le point hors du terrain")
+            return False
 
 
 
