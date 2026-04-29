1582768b14bbda345afd7f1ad71b948da5a1fbf0
Xandeers <nedim.tr.pro@gmail.com>
2026-04-29 16:45:50 +0200

    obj graph created

---
+++ terrain/graph.py
@@ -0,0 +1,18 @@
+import cv2
+import numpy as np
+import json 
+import os
+
+
+
+class Graph:
+    def __init__(self, x_widthCM=301, y_lengthCM=390):
+
+        self.width=x_widthCM
+        self.length=y_lengthCM
+        self.point=[]
+
+
+
+
+
