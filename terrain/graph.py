5e98c1a62637b45bf8860a43e7805166c52eaa85
Xandeers <nedim.tr.pro@gmail.com>
2026-04-29 18:43:58 +0200

    function config matrice

--- terrain/graph.py
+++ terrain/graph.py
@@ -21,10 +21,38 @@ class Graph:
     def __init__(self, x_widthCM=301, y_lengthCM=390):
         self.width=x_widthCM
         self.length=y_lengthCM
-        self.point=[]
+        self.matrice_cam1=None
+        self.matrice_cam2=None
     
+
+
+    def matriceConfig(self,pts_images,pts_graph,id_cam):
+        """prend une liste de point du retour camera ainsi que leur correspondance dans le graph 
+          permet de calcluler la matrice qui permet de faire correspondre un point du plan cam choisi"""
+        
+        # pts_image: coordonnéés pixels [(x1,y1), (x2,y2)...]
+        # pts_graph: coordonnées cm [(0,0), (301,0)...]
+        pts_src=np.array(pts_images, dtype=float)
+        pts_dst=np.array(pts_graph, dtype=float)
+
+        matrice,_= cv2.findHomography(pts_src,pts_dst)
+
+        if id_cam == 1 : 
+            self.matrice_cam1 = matrice
+        
+        if id_cam == 2:
+            self.matrice_cam2 = matrice
+        
+
+
+
+
+
+
+
     
     def add_point(self,p):
+        """verifie si le point est dans le terrain et l'ajoute à la liste de pts"""
         if 0<= p.x <= self.width and 0<= p.y <= self.length :
             self.point.append(p)
         else:
@@ -38,6 +66,7 @@ class Graph:
         else:
             return False
         
+    
 
 
 
