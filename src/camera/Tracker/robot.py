9c2c7ea9cfab779ad15021d1af0b2273f3d6bdab
Xandeers <nedim.tr.pro@gmail.com>
2026-05-01 19:20:18 +0200

    getposition feature added

--- src/camera/Tracker/robot.py
+++ src/camera/Tracker/robot.py
@@ -8,3 +8,38 @@ class RobotTracker :
     
         self.lower_green = np.array([35, 100, 50])   
         self.upper_green = np.array([85, 255, 255])
+
+
+    def getposition(self, frame):
+        """Analyse l'image et renvoie (x, y) du robot ou None. """
+
+        hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+
+        mask = cv2.inRange(hsv, self.lower_green, self.upper_green)
+
+        kernel = np.ones((5, 5), np.uint8)
+        mask = cv2.erode(mask, kernel, iterations=1)
+        mask = cv2.dilate(mask, kernel, iterations=1)
+    
+    # Debug pour voir si le robot est bien "blanc" sur le masque
+        cv2.imshow("Debug Masque Robot", mask)
+
+        contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    
+        robot_data = None 
+
+        if contours:
+        
+            largest_contour = max(contours, key=cv2.contourArea)
+    
+            ((x, y), radius) = cv2.minEnclosingCircle(largest_contour)
+
+        # On ajuste le rayon minimum selon la taille réelle de ton robot à l'image
+        if radius > 5: # Augmenté un peu pour éviter les faux positifs
+            robot_data = {
+                "center": (int(x), int(y)),
+                "radius": int(radius),
+                "area": cv2.contourArea(largest_contour)
+            }
+    
+        return robot_data
