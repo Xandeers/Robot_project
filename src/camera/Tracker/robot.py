8556f0edd3877a041c65401dcd60124766beff1a
Xandeers <nedim.tr.pro@gmail.com>
2026-05-01 20:10:26 +0200

    class modification

--- src/camera/Tracker/robot.py
+++ src/camera/Tracker/robot.py
@@ -1,59 +1,70 @@
 import cv2
+import math
 import numpy as np 
 
-
-
-class RobotTracker :
+class RobotTracker:
     def __init__(self):
-    
+        
         self.lower_green = np.array([35, 100, 50])   
         self.upper_green = np.array([85, 255, 255])
-
+        
+        self.lower_blue = np.array([100, 100, 50])
+        self.upper_blue = np.array([130, 255, 255])
 
     def getposition(self, frame):
-        """Analyse l'image et renvoie (x, y) du robot ou None. """
-
         hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-
-        mask = cv2.inRange(hsv, self.lower_green, self.upper_green)
-
         kernel = np.ones((5, 5), np.uint8)
-        mask = cv2.erode(mask, kernel, iterations=1)
-        mask = cv2.dilate(mask, kernel, iterations=1)
-    
-     #Debug pour voir si le robot est bien "blanc" sur le masque
-        cv2.imshow("Debug Masque Robot", mask)
 
-        contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    
-        robot_data = None 
+        def find_color_center(mask_in):
+            mask_in = cv2.erode(mask_in, kernel, iterations=1)
+            mask_in = cv2.dilate(mask_in, kernel, iterations=1)
+            cnts, _ = cv2.findContours(mask_in, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+            if cnts:
+                largest = max(cnts, key=cv2.contourArea)
+                if cv2.contourArea(largest) > 50:
+                    M = cv2.moments(largest)
+                    if M["m00"] != 0:
+                        return (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
+            return None
 
-        if contours:
         
-            largest_contour = max(contours, key=cv2.contourArea)
-    
-            ((x, y), radius) = cv2.minEnclosingCircle(largest_contour)
+        mask_v = cv2.inRange(hsv, self.lower_green, self.upper_green)
+        mask_b = cv2.inRange(hsv, self.lower_blue, self.upper_blue)
 
-        # On ajuste le rayon minimum selon la taille réelle de ton robot à l'image
-        if radius > 5: # Augmenté un peu pour éviter les faux positifs
+        p_avant = find_color_center(mask_v)
+        p_arriere = find_color_center(mask_b)
+
+        robot_data = None
+
+        if p_avant and p_arriere:
+           
+            center = ((p_avant[0] + p_arriere[0]) // 2, (p_avant[1] + p_arriere[1]) // 2)
+            
+            
+            angle = math.degrees(math.atan2(p_avant[1] - p_arriere[1], p_avant[0] - p_arriere[0]))
+
             robot_data = {
-                "center": (int(x), int(y)),
-                "radius": int(radius),
-                "area": cv2.contourArea(largest_contour)
+                "center": center,
+                "front": p_avant,
+                "angle": angle
             }
     
         return robot_data
 
-
-    def drawRobot(self,frame,robot_data):
-
-        if robot_data :
+    
+    def drawRobot(self, frame, robot_data):
+        if robot_data:
             center = robot_data["center"]
-            display_radius = max(robot_data["radius"],10)
-        
-            cv2.circle(frame, center, display_radius, (0, 255, 0), 3)
+            front = robot_data["front"]
             
-            #Centre en ROUGE (-1 pour remplir le cercle)
-            cv2.circle(frame, center, 5, (0, 0, 255), -1)
             
+            cv2.circle(frame, center, 20, (255, 255, 255), 2)
+            
+           
+            cv2.arrowedLine(frame, center, front, (0, 255, 0), 3, tipLength=0.3)
+            
+            
+            cv2.putText(frame, f"{int(robot_data['angle'])} deg", (center[0]+25, center[1]), 
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 1)
+                
         return frame
