d7a175c491da0c5b7caf67e7e6dabe2a0b23a1d3
Xandeers <nedim.tr.pro@gmail.com>
2026-05-22 00:07:58 +0200

    left spin implemented

--- test/robot_test/main_test.py
+++ test/robot_test/main_test.py
@@ -30,6 +30,10 @@ def executer_moteurs(action):
         rotation_Droite(mAVD, mAVG, mARD, mARG)
 
     elif action == 2:
+        print("MOTEURS : PIVOTE DROITE")
+        rotation_Droite(mAVD, mAVG, mARD, mARG)
+
+    elif action == 3:
         print("MOTEURS : STOP")
         stopVehicule(mAVD, mAVG, mARD, mARG)
 
