a542c3e7ac654238cf338febbf957ba2159b31ed
Xandeers <nedim.tr.pro@gmail.com>
2026-05-25 17:48:10 +0200

    envoyer function added

--- supervison/server.py
+++ supervison/server.py
@@ -10,3 +10,19 @@ Socketio = SocketIO(app, cors_allowed_origins="*")
 def index():
     """Page principal dashbord"""
     return render_template('index.html')
+
+def envoye(robot_x, robot_y, robot_angle, ball_x, ball_y, phase, ordre):
+    """gere l'envoye des donnée """
+
+    data = {
+        "robot_x": robot_x,
+        "robot_y": robot_y,
+        "robot_angle": robot_angle,
+        "ball_x": ball_x,
+        "ball_y":ball_y,
+        "pahse": phase,
+        "ordre": ordre
+    }
+
+    Socketio.emit('update',data)
+
